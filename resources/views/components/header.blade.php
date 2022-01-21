6005783e24eda8e1385ed7311eb9f2de01ad1cfd
Pier <pierfrancesco297@gmail.com>
2022-01-21 16:43:28 +0100

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,21 @@
+<div id="other-sites">
+
+</div>
 <header>
-    <h2>sono l'header</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1200px-DC_Comics_logo.svg.png" alt="DC-Logo">
+    <nav>
+        <ul>
+            <li><a href="#">CHARACTERS</a> </li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+    </nav>
+    <input type="text" placeholder="Search..">
 </header>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Laravel comics</title>
 </head>
 <body>
@@ -11,7 +12,7 @@
     <section>
         @include('components.header')
         <h1>sono il main</h1>
-        @yield('content')
+        {{-- @yield('content') --}}
         @include('components.footer')
     </section>
 
