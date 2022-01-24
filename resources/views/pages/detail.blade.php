daeefffca92985c010daf4a3ace9f31e3017777d
Pier <pierfrancesco297@gmail.com>
2022-01-24 18:06:22 +0100

    detail page descrizione prodotto

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -5,5 +5,41 @@
     <div id="jumbo">
 
     </div>
-    <div class="comic-main"></div>
+    <div class="comic-banner">
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
+    </div>
+    <div class="comic-main">
+        {{-- descrizione prodotto --}}
+        <div>
+        <h3>ACTION COMICS #1000: THE DELUXE EDITION</h3>
+        <div class="green-btn first-green">
+            <div>
+            <span>U.S. Price:</span>
+            <span>$19.99</span>
+            </div>
+            <div>
+                <span>AVAILABLE</span>
+            </div>
+        </div>
+        <div class="green-btn second-green">
+            <div>
+            <span> Check Availability</span>
+        </div>
+        
+        </div>
+        <div class="description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo minima sit esse assumenda earum soluta numquam error fuga
+                dolorum molestiae asperiores quas velit voluptates natus vero impedit placeat, iste saepe sequi eum dolores vitae veritatis
+                a vitae maxime ipsum, similique id velit eum expedita eveniet totam ab aspernatur doloremque culpa sint, molestias, libero deserunt
+                quod ullam nobis reiciendis fuga perspiciatis cupiditate placeat officia. Nulla, veniam quod velit culpa commodi alias error itaque
+                voluptates provident praesentium cumque unde ipsam quidem? Odit saepe reiciendis voluptas soluta voluptate eos sunt ad repellendus
+                , atque cum quia laborum dolorem, consequatur veniam repellat quas. Repellat quaerat accusamus doloremque architecto tempora sint
+                distinctio excepturi, consectetur dolorem officiis, vel cumque, eius numquam. Doloremque accusantium repellat quas similique ea.
+            </p>
+        </div>
+    </div>
+
+    {{-- advertisement --}}
+    <div></div>
+    </div>
 @endsection
